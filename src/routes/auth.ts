f69aa3a431a7609c39f44294cfd5e7aff63f4447
Paulo Roberto <contato.paulorobertodev@gmail.com>
2026-02-03 13:11:10 -0300

    refactor: user registration endpoint without org creation

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,6 +3,6 @@ import * as authController from "../controllers/authController";
 
 const router = Router();
 
-router.post("/login", authController.login);
+router.post("/register", authController.register);
 
 export default router;
